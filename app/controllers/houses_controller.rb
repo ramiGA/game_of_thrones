536e3a4f4fe85c3182b557e8e59a1c68cbcaab8a
ramiGA <ramisn3@gmail.com>
2017-08-02 23:21:20 -0400

    finish house cont

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -3,17 +3,47 @@ class HousesController < ApplicationController
     @houses = House.all
   end
 
-
+  # new
   def new
+    @house = House.new
   end
 
-  def edit
+  # create
+  def create
+    @house = House.create!(house_params)
+
+    redirect_to house_path(@house)
   end
-  
+
+  #show
   def show
     @house = House.find(params[:id])
   end
 
-  def delete
+  # edit
+  def edit
+    @house = House.find(params[:id])
+  end
+
+
+  # update
+  def update
+    @house = House.find(params[:id])
+    @house.update(house_params)
+
+    redirect_to house_path(@house)
+  end
+
+  # destroy
+  def destroy
+    @house = House.find(params[:id])
+    @house.destroy
+
+    redirect_to houses_path
+  end
+
+  private
+  def house_params
+    params.require(:house).permit(:name, :title)
   end
 end
