07ac33f6ec12a88f0be8e769f4b92013e2cc271e
ramiGA <ramisn3@gmail.com>
2017-08-03 07:38:31 -0400

    completed charaters

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,19 +1,48 @@
 class CharactersController < ApplicationController
-  def index
-    @characters = Character.all
-  end
+  # new
+def new
+  @house = House.find(params[:house_id])
+  @character = @house.characters.new
+end
 
+# create
+def create
+  @house = House.find(params[:house_id])
+  @character = @house.characters.create!(character_params)
 
-  def new
-  end
+  redirect_to house_path(@house)
+end
 
-  def edit
-  end
-  
-  def show
-    @character = Character.find(params[:id])
-  end
+#show
+def show
+  @character = Character.find(params[:id])
+end
 
-  def delete
-  end
+# edit
+def edit
+  @house = Character.find(params[:character_id])
+  @character = Character.find(params[:id])
+end
+
+# update
+def update
+  @house = House.find(params[:house_id])
+  @character = Character.find(params[:id])
+  @character.update(character_params)
+
+  redirect_to house_character_path(@house, @character)
+end
+
+# destroy
+def destroy
+  @character = Character.find(params[:id])
+  @character.destroy
+
+  redirect_to house_path(@character.house)
+end
+
+private
+def character_params
+  params.require(:character).permit(:description, :name, :house_id)
+end
 end
